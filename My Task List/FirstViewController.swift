75263c4938e8fb78457290d6e79001b1b82c9a87
Bjarte Skjørestad <bjarteskjorestad@Bjartes-Mac.local>
2014-06-29 06:24:54 -0700

    Initial Commit

---
+++ My Task List/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  My Task List
+//
+//  Created by Bjarte Skjørestad on 29/06/14.
+//  Copyright (c) 2014 Bjarte Skjørestad. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
